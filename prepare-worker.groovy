252c7e95380ae5cf48bf1039793373870a2a7758
Islam Kambarov <ikambarov@yahoo.com>
2020-08-26 00:12:02 +0000

    Updated prepare-worker

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,9 +1,13 @@
 node {
-    stage('Init') {
-        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        stage('Init') {
             sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.131.115.78 yum install epel-release -y'
+        }
+        stage("Install git") {
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.131.115.78 yum install git -y'
+        }
+        stage("Install Java"){
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.131.115.78 yum install java-1.8.0-openjdk-devel -y'
         }
     }
 }
-
-
