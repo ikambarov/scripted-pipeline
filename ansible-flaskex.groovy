2bddf35bb3a05221d03256b85aadb5de5a84645d
Islam Kambarov <ikambarov@yahoo.com>
2020-08-28 01:39:10 +0000

    ansible-flaskex v1

---
+++ ansible-flaskex.groovy
@@ -0,0 +1,24 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true)
+        ])
+    ])
+
+if (nodeIP.length() > 6) {
+    node {
+        stage('Pull Repo') {
+            git branch: 'master', changelog: false, poll: false, url: 'https://github.com/ikambarov/ansible-Flaskex.git'
+        }
+
+        withEnv(['ANSIBLE_HOST_KEY_CHECKING=False', 'FLASKEX_REPO=https://github.com/ikambarov/Flaskex.git', 'FLASKEX_BRANCH=master']) {
+            stage("Install Apache"){
+                ansiblePlaybook credentialsId: 'jenkins-master-ssh-key', inventory: '${nodeIP},', playbook: 'main.yml'
+                }
+            }  
+        }  
+}
+else {
+    error 'Please enter valid IP address'
+}
+
+
