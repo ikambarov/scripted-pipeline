6a949d79e87b90caef838c1ce0b689e53ee3ab02
Islam Kambarov <ikambarov@yahoo.com>
2020-08-29 22:20:20 +0000

    Fix toBoolean() issue

--- terraform-vpc.groovy
+++ terraform-vpc.groovy
@@ -1,3 +1,10 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: true, description: 'Do you want to run terrform apply', name: 'terraform_apply'),
+        booleanParam(defaultValue: false, description: 'Do you want to run terrform destroy', name: 'terraform_destroy')
+    ])
+])
+
 node{
     stage("Pull Repo"){
         git url: 'https://github.com/ikambarov/terraform-vpc.git'
@@ -11,12 +18,30 @@ node{
                 terraform-0.13 init
                 terraform-0.13 plan -var-file dev.tfvars
             '''
+        }        
+        
+        if (terraform_apply.toBoolean()) {
+            stage("Terraform Apply"){
+                sh '''
+                    export AWS_REGION=us-east-1
+                    terraform-0.13 apply -var-file dev.tfvars -auto-approve
+                '''
+            }
         }
-        stage("Terraform Apply"){
-            sh '''
-                export AWS_REGION=us-east-1
-                terraform-0.13 apply -var-file dev.tfvars -auto-approve
-            '''
+        else if (terraform_destroy.toBoolean()) {
+            stage("Terraform Destroy"){
+                sh '''
+                    export AWS_REGION=us-east-1
+                    terraform-0.13 destroy -var-file dev.tfvars -auto-approve
+                '''
+            }
+        }
+        else {
+            stage("Uknown"){
+                sh '''
+                    echo "Choose either apply or destroy"
+                '''
+            }
         }
     }    
 }
