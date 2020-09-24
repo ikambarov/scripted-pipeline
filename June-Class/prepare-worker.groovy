42d53a8809156662c27b8d20dfd404db876d7573
Islam Kambarov <ikambarov@yahoo.com>
2020-09-24 01:07:24 +0000

    configured parameter for node IP

--- June-Class/prepare-worker.groovy
+++ June-Class/prepare-worker.groovy
@@ -1,7 +1,13 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Input node IP', name: 'SSHNODE', trim: true)
+        ])
+    ])
+
 node {
     stage("Initialize") {
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@159.203.117.71 yum install epel-release -y "
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@$params.SSHNODE yum install epel-release -y "
         }
     }
 }
