1fcc1ba7356152ffddbc6c6b12c8b8bb25cc431f
Islam Kambarov <ikambarov@yahoo.com>
2020-09-28 23:19:37 +0000

    install terraform

--- June-Class/prepare-worker.groovy
+++ June-Class/prepare-worker.groovy
@@ -18,5 +18,8 @@ node {
         stage("Install Ansible") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install ansible -y"
         }
+        stage("Install Terraform") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } 'yum install -y wget unzip && wget https://releases.hashicorp.com/terraform/0.13.1/terraform_0.13.1_linux_amd64.zip && unzip terraform_0.13.1_linux_amd64.zip && mv terraform /usr/bin/'"
+        }
     }
 }
