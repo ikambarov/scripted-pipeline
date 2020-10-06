b9781b83ba0f17fede7d011ecd279649ae9b4cf0
Islam Kambarov <ikambarov@yahoo.com>
2020-10-06 02:00:27 +0000

    Interpolate username

--- June-Class/docker-flaskex.groovy
+++ June-Class/docker-flaskex.groovy
@@ -2,18 +2,20 @@ node("docker"){
     stage("Pull Repo"){
         git url: 'https://github.com/ikambarov/Flaskex-docker.git'
     }
-
-    stage("Docker Build"){
-        sh 'docker build -t ikambarov/flaskex  .'
-    }
-
+    
     withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'REGISTRY_PASSWORD', usernameVariable: 'REGISTRY_USERNAME')]) {
+        stage("Docker Build"){
+            sh "docker build -t ${REGISTRY_USERNAME}/flaskex  ."
+        }
+        
         stage("Docker Login"){
             sh "docker login -u ${REGISTRY_USERNAME} -p '${REGISTRY_PASSWORD}'"
         }
+
+        stage("Docker Push"){
+            sh "docker push ${REGISTRY_USERNAME}/flaskex"
+        }
     }
 
-    stage("Docker Push"){
-        sh 'docker push ikambarov/flaskex'
-    }
+    
 }
