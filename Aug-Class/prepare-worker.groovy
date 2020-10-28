f189e2f7754aa4aabce3476495290e304fe844dc
Islam Kambarov <ikambarov@yahoo.com>
2020-10-28 00:52:35 +0000

    updated version of prepare worker

--- Aug-Class/prepare-worker.groovy
+++ Aug-Class/prepare-worker.groovy
@@ -1,9 +1,19 @@
 node{
-    stage('Initialize'){
-        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            sh '''
-                    ssh -i $SSHKEY root@104.131.114.15 yum install epel-release -y
-                '''
+    properties([
+        parameters([
+            string(defaultValue: '', description: 'Provide Remote Node IP', name: 'node_ip', trim: true)
+        ])
+    ])
+
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-ssh-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
+        stage('Install Prerequisites'){
+            sh "ssh -o StrictHostKeyChecking=false -i $SSH_KEY root@${node_ip} yum install epel-release -y"
+        }
+        stage('Install Git'){
+            sh "ssh -o StrictHostKeyChecking=false -i $SSH_KEY root@${node_ip} yum install git -y"
+        }
+        stage('Install Java'){
+            sh "ssh -o StrictHostKeyChecking=false -i $SSH_KEY root@${node_ip} yum install java-1.8.0-openjdk-devel -y"
         }
     } 
 }
