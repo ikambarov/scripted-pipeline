80eb20fc0c5651e8ea0b5de11efe5f0abd8436f3
Islam Kambarov <ikambarov@yahoo.com>
2020-10-31 20:32:17 +0000

    install-melodi.groovy was created

---
+++ Aug-Class/install-melodi.groovy
@@ -0,0 +1,20 @@
+node{
+    properties([
+        parameters([
+            string(defaultValue: '', description: 'Provide Remote Node IP', name: 'node_ip', trim: true)
+        ])
+    ])
+
+    stage("Git Pull"){
+        git url: 'https://github.com/ikambarov/ansible-melodi.git'
+    }
+
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-ssh-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
+       stage("Ping"){
+            sh """
+                export ANSIBLE_HOST_KEY_CHECKING=False
+                ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME main.yml
+            """
+        } 
+    }  
+}
