de3e3876a5e80791c14414cb68a83b7880029d5c
Islam Kambarov <ikambarov@yahoo.com>
2020-10-31 21:19:17 +0000

    Split to multiple stages

--- Aug-Class/ansible-flaskex.groovy
+++ Aug-Class/ansible-flaskex.groovy
@@ -9,16 +9,34 @@ node{
         stage("Git Pull"){
             git url: 'https://github.com/ikambarov/ansible-Flaskex.git'
         }
-
         
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-ssh-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
-            stage("Ping"){
+            stage("Instll Prerequisites"){
+                sh """
+                    export ANSIBLE_HOST_KEY_CHECKING=False
+                    ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME prerequisites.yml
+                """ 
+            } 
+            stage("Pull repo"){
                 withEnv(['FLASKEX_REPO=https://github.com/ikambarov/Flaskex.git', 'FLASKEX_BRANCH=master']) {
                     sh """
                         export ANSIBLE_HOST_KEY_CHECKING=False
-                        ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME main.yml
+                        ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME pull_repo.yml
                     """
                 }     
+            } 
+            stage("Install Python"){
+                sh """
+                    export ANSIBLE_HOST_KEY_CHECKING=False
+                    ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME install_python.yml
+                """    
+            } 
+            stage("Start App"){
+                sh """
+                    export ANSIBLE_HOST_KEY_CHECKING=False
+                    ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME start_app.yml
+
+                """    
             } 
         }  
     }
