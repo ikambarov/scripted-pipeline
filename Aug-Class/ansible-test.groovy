63128472baae0fdbbe5e68e24d3a6602ad0c30a9
Islam Kambarov <ikambarov@yahoo.com>
2020-10-31 20:32:04 +0000

    ANSIBLE_HOST_KEY_CHECKING=False

--- Aug-Class/ansible-test.groovy
+++ Aug-Class/ansible-test.groovy
@@ -1,8 +1,15 @@
 node{
+    properties([
+        parameters([
+            string(defaultValue: '', description: 'Provide Remote Node IP', name: 'node_ip', trim: true)
+        ])
+    ])
+
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-ssh-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
        stage("Ping"){
             sh """
-                ansible -i "162.243.167.53," --private-key $SSH_KEY all -m ping -u $SSH_USERNAME
+                export ANSIBLE_HOST_KEY_CHECKING=False
+                ansible -i "${node_ip}," --private-key $SSH_KEY all -m ping -u $SSH_USERNAME
             """
         } 
     }  
