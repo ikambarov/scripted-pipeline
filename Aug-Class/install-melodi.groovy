9da0701d1c3645c2678a26d0be3772f51957b1d6
Islam Kambarov <ikambarov@yahoo.com>
2020-10-31 20:58:57 +0000

    Check input

--- Aug-Class/install-melodi.groovy
+++ Aug-Class/install-melodi.groovy
@@ -5,16 +5,22 @@ node{
         ])
     ])
 
-    stage("Git Pull"){
-        git url: 'https://github.com/ikambarov/ansible-melodi.git'
-    }
+    if(node_ip.length() > 7 ){
+        stage("Git Pull"){
+            git url: 'https://github.com/ikambarov/ansible-melodi.git'
+        }
 
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-ssh-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
-       stage("Ping"){
-            sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME main.yml
-            """
-        } 
-    }  
+        
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-ssh-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
+        stage("Ping"){
+                sh """
+                    export ANSIBLE_HOST_KEY_CHECKING=False
+                    ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME main.yml
+                """
+            } 
+        }  
+    }
+    else {
+        error("Please provide a valid IP")
+    }
 }
