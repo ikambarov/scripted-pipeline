f667bf885b1680726d9d4003e78dbc21cc9a3fee
Islam Kambarov <ikambarov@yahoo.com>
2020-11-16 20:09:47 -0600

    k8-tools job was created

---
+++ June-Class/k8-tools.groovy
@@ -0,0 +1,39 @@
+
+def pod_name =  "jenkins-agent-${UUID.randomUUID().toString()}"
+
+def pod_yaml = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: jenkins-agent
+  name: jenkins-agent
+spec:
+  affinity:
+    podAntiAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+      - labelSelector:
+        matchExpressions:
+        - key: app.kubernetes.io/component
+          operator: In
+          values:
+          - jenkins-master
+        topologyKey: "kubernetes.io/hostname"
+  containers:
+  - image: ikambarov/jnlp-agent-k8-tools
+    name: k8-tools 
+  serviceAccount: k8-tools
+'''
+
+podTemplate(label: pod_name, name: pod_name, namespace: 'default', yaml: pod_yaml) {
+    node(pod_name){
+        container('k8-tools'){
+            stage("Kubernetes"){
+                sh '''
+                    kubectl get pv
+                    helm list
+                '''
+            }
+        }
+    }
+}
