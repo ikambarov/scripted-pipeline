3ef9b2dc953632fab9f84cd11879828b1554228d
Islam Kambarov <ikambarov@yahoo.com>
2020-12-30 03:17:24 +0000

    v2 - still broken

--- Aug-2020/terraform-vpc.groovy
+++ Aug-2020/terraform-vpc.groovy
@@ -1,23 +1,57 @@
+properties([
+    parameters([
+        choice(choices: ['Plan', 'Apply', 'Destroy'], description: '', name: 'ACTION'),
+        choice(choices: ['Dev', 'QA', 'Prod'], description: '', name: 'ENVIRONMENT')
+        ])
+    ])
+
 node("terraform"){
+
+    def vpc_env='dev'
+    def vpc_region='us-east-1'
+    if(params.ENVIRONMENT == 'QA'){
+        vpc_env='qa'
+        vpc_region='us-east-2'
+    }
+    else if (params.ENVIRONMENT == 'Prod'){
+        vpc_env='prod'
+        vpc_region='us-west-2'
+    }
+
     stage("Pull Repo"){
         git url: 'https://github.com/ikambarov/terraform-vpc.git'
     }
 
     withCredentials([usernamePassword(credentialsId: 'jenkins_aws_keys', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        withEnv(['AWS_REGION=us-east-1']) {
+        withEnv(['AWS_REGION=${vpc_region}']) {
             stage("Terrraform Init"){
                 sh '''
-                    source setenv.sh dev.tfvars
+                    source setenv.sh ${vpc_env}.tfvars
                     terraform init
-                    terraform plan -var-file dev.tfvars
                 '''
             }        
             
-            stage("Terraform Apply"){
-                sh '''
-                    terraform apply -var-file dev.tfvars -auto-approve
-                '''
+            if(params.ACTION == 'Destroy'){
+                stage("Terraform Destroy"){
+                    sh '''
+                        terraform destroy -var-file ${vpc_env}.tfvars -auto-approve
+                    '''
+                }
             }
-        }        
+            else if(params.ACTION == 'Apply'){
+                stage("Terraform Apply"){
+                    sh '''
+                        terraform apply -var-file ${vpc_env}.tfvars -auto-approve
+                    '''
+                }
+            }
+            else if(params.ACTION == 'Plan') {
+                stage("Terraform Plan"){
+                    sh '''
+                        terraform plan -var-file ${vpc_env}.tfvars
+                    '''
+                }
+            }       
+        }
     }    
 }
