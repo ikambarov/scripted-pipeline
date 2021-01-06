10afe1baa9639ffc933f231ac4b398a0e0bc425c
Islam Kambarov <ikambarov@yahoo.com>
2021-01-06 01:36:27 +0000

    Parameterized

--- Aug-2020/packer.groovy
+++ Aug-2020/packer.groovy
@@ -1,10 +1,33 @@
+properties([
+    parameters([
+        choice(choices: ['dev', 'qa', 'prod'], description: 'Choose Environment', name: 'environment')
+    ])
+])
+
+def aws_region_var = ''
+
+if(params.environment == "dev") {
+    println("Creating image at Dev")
+    aws_region_var = "us-east-1"
+}
+else if(params.environment == "qa") {
+    println("Creating image at QA")
+    aws_region_var = "us-east-2"
+}
+else if(params.environment == "prod") {
+    println("Creating image at Prod")
+    aws_region_var = "us-west-2"
+}
+
+def random_name = "${params.environment}-${ UUID.randomUUID().toString()}"
+
 node("packer"){
     stage('Pull Repo') {
         git url: 'https://github.com/ikambarov/packer.git'
     }
 
     withCredentials([usernamePassword(credentialsId: 'jenkins_aws_keys', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        withEnv(['AWS_REGION=us-east-1', 'PACKER_AMI_NAME=random-name']) {
+        withEnv(["AWS_REGION=${aws_region_var}", "PACKER_AMI_NAME=${random_name}"]) {
             stage('Packer Validate') {
                 sh 'packer validate apache.json'
             }
