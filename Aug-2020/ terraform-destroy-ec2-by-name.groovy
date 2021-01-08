ad8a0de845718c6d29946feabe4f195fbd37acc7
Islam Kambarov <ikambarov@yahoo.com>
2021-01-08 03:11:19 +0000

    Final version

--- Aug-2020/ terraform-destroy-ec2-by-name.groovy
+++ Aug-2020/ terraform-destroy-ec2-by-name.groovy
@@ -20,7 +20,7 @@ node("terraform"){
 
     def tfvar = """
         s3_bucket = "jenkins-terraform-evolvecybertraining"
-        s3_folder_project = "terraform-ec2-by-name"
+        s3_folder_project = "terraform_ec2"
         s3_folder_region = "us-east-1"
         s3_folder_type = "class"
         s3_tfstate_file = "infrastructure.tfstate"
@@ -28,7 +28,7 @@ node("terraform"){
         environment = "${ec2_env}"
         region      = "${ec2_region}"
         public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXUI8Mt0W/65CPA5rnR4auE8qVb08c6qR9Ca0yQaz9xM6EuShYX8jmktYbrdCIkZTMXbRF58CkWID/NHjYX4ZWZHwLi5uf2RfQegF67+kv6yJ2cgG4AsxUmWqlznxvm9615r8tpzBkKgsya58H+4aPRKqLJmhRm3ZZCa7t2HE7S+RR7fq+WtaQ3BMaKog9AVfHSEP8Gp4Ho7WUv5YlLXu5hlYC+m2oxrSCqXRFIhDtDuyphkzS93gDy8EVBkWnJFkoXT2LbVydcJaNCpEdjB1YFEEc1kMOXCAZ0w5N8PiWgdlY0lPeRXdH1RLX+WCM5FVOT9ujrq8PTQSYIkl2pek3 ikambarov@Islams-MacBook-Pro.local"
-        ami_name    = "${params.AMI_NAME}"
+        ami_name    = \"*\"
     """
 
     stage("Pull Repo"){
@@ -44,7 +44,7 @@ node("terraform"){
                     terraform init
                 """
             }
-            
+
             stage("Terraform Destroy"){
                 sh """
                     terraform destroy -var-file ${ec2_env}.tfvars -auto-approve
--- Aug-2020/terraform-ec2-by-name.groovy
+++ Aug-2020/terraform-ec2-by-name.groovy
@@ -22,7 +22,7 @@ node("terraform"){
 
     def tfvar = """
         s3_bucket = "jenkins-terraform-evolvecybertraining"
-        s3_folder_project = "terraform-ec2-by-name"
+        s3_folder_project = "terraform_ec2"
         s3_folder_region = "us-east-1"
         s3_folder_type = "class"
         s3_tfstate_file = "infrastructure.tfstate"
