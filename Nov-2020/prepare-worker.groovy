be10c7de165a01eb31cb980af4d8a8ed69f681fc
Islam Kambarov <ikambarov@yahoo.com>
2021-01-30 18:44:53 +0000

    Updated create worker

--- Nov-2020/prepare-worker.groovy
+++ Nov-2020/prepare-worker.groovy
@@ -1,5 +1,19 @@
+properties([
+    parameters([
+            string(defaultValue: '', description: 'Enter IP Address', name: 'IP', trim: true)
+        ])
+    ])
+
 node {
-    stage('Stage #1'){
-        sh "echo 'Hello World'"
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
+        stage('Install Preprequisites'){
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@${ params.IP } 'yum install epel-release -y' "
+        }
+        stage('Install Java'){
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@${ params.IP } 'yum install java-1.8.0-openjdk-devel -y' "
+        }
+        stage('Install git'){
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@${ params.IP } 'yum install git -y' "
+        }
     }
 }
