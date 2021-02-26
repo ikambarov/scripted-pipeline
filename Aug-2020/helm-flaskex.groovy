5b19ee5064e94632b3fd70607bc03f6c03a78860
Islam Kambarov <ikambarov@yahoo.com>
2021-02-25 19:49:50 -0600

    helm-flaskex.groovy was created

---
+++ Aug-2020/helm-flaskex.groovy
@@ -0,0 +1,40 @@
+def k8slabel = "jenkins-agent"
+
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: k8-tools
+          image: ikambarov/k8-tools
+          imagePullPolicy: Always
+        serviceAccountName: k8-tools
+    """
+
+podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+    node(k8slabel) {
+        stage("Pull code") {
+            container("k8-tools") {
+                git 'https://github.com/ikambarov/flaskex-chart.git'
+            }
+        }
+        stage("Helm install") {
+            container("k8-tools") {
+                sh 'helm install -n default flaskex .'
+            }
+        }
+    }
+}
