dd405e3a53c7bf2228214365d7ce3799c634a190
Islam Kambarov <ikambarov@yahoo.com>
2021-04-10 16:10:39 +0000

    terraform-ec2.groovy was created

---
+++ Nov-2020/terraform-ec2.groovy
@@ -0,0 +1,77 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: false, description: 'Do you want to run Terraform apply?', name: 'terraform_apply'),
+        booleanParam(defaultValue: false, description: 'Do you want to run Terraform destroy?', name: 'terraform_destroy'),
+        choice(choices: ['dev', 'qa', 'prod'], description: 'Choose environment: ', name: 'environment'),
+        string(defaultValue: '', description: 'Provide AMI ID', name: 'ami_id', trim: true)
+        ])
+    ])
+node{
+    def aws_region_var = ''
+
+    if(params.environment == 'dev'){
+        println("Applying for dev")
+        aws_region_var = 'us-east-1'
+    }
+    else if(params.environment == 'qa'){
+        println("Applying for qa")
+        aws_region_var = 'us-east-2'
+    }
+    else{
+        println("Applying for prod")
+        aws_region_var = 'us-west-2'
+    }
+
+    def tfvar = """
+    s3_bucket = "jenkins-terraform-evolvecybertraining"
+    s3_folder_project = "terraform_ec2"
+    s3_folder_region = "us-east-1"
+    s3_folder_type = "class"
+    s3_tfstate_file = "infrastructure.tfstate"
+    
+    environment = "${params.environment}"
+    region      = "${aws_region_var}"
+    public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXUI8Mt0W/65CPA5rnR4auE8qVb08c6qR9Ca0yQaz9xM6EuShYX8jmktYbrdCIkZTMXbRF58CkWID/NHjYX4ZWZHwLi5uf2RfQegF67+kv6yJ2cgG4AsxUmWqlznxvm9615r8tpzBkKgsya58H+4aPRKqLJmhRm3ZZCa7t2HE7S+RR7fq+WtaQ3BMaKog9AVfHSEP8Gp4Ho7WUv5YlLXu5hlYC+m2oxrSCqXRFIhDtDuyphkzS93gDy8EVBkWnJFkoXT2LbVydcJaNCpEdjB1YFEEc1kMOXCAZ0w5N8PiWgdlY0lPeRXdH1RLX+WCM5FVOT9ujrq8PTQSYIkl2pek3 ikambarov@Islams-MacBook-Pro.local"
+    ami_id      = "${params.ami_id}"
+    """
+
+    stage("Pull Repo"){
+        cleanWs()
+        git url: 'https://github.com/ikambarov/terraform-ec2.git'
+        writeFile file: "${params.environment}.tfvars", text: "${tfvar}"
+    }
+
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        withEnv(["AWS_REGION=${aws_region_var}"]) {
+            stage("Terrraform Init"){
+                sh """
+                    bash setenv.sh ${params.environment}.tfvars
+                    terraform init
+                    terraform plan -var-file dev.tfvars
+                """
+            }        
+            
+            if(params.terraform_apply){
+                stage("Terraform Apply"){
+                    sh """
+                        terraform apply -var-file ${params.environment}.tfvars -auto-approve
+                    """
+                }
+            }
+            else if(params.terraform_destroy){
+                stage("Terraform Destroy"){
+                    sh """
+                        terraform destroy -var-file ${params.environment}.tfvars -auto-approve
+                    """
+                }
+            }
+            else {
+                stage("Terraform Plan"){
+                    sh """
+                        terraform plan -var-file ${environment}.tfvars
+                    """
+                }
+            }
+        }        
+    }    
+}
