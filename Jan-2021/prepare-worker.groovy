74722d6264e8971716944b796a0625b17065a78e
Islam Kambarov <ikambarov@yahoo.com>
2021-04-14 01:07:16 +0000

    Add install java stage

--- Jan-2021/prepare-worker.groovy
+++ Jan-2021/prepare-worker.groovy
@@ -1,8 +1,13 @@
 node{
-    stage("Install git"){
-        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        stage("Install git"){
             sh """
                 ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.236.193.148 yum install git -y
+            """
+        }   
+        stage("Install Java"){
+            sh """
+                ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.236.193.148 yum install java-1.8.0-openjdk-devel -y
             """
         }   
     }
