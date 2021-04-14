d0d829fd98ba584df169acdde52fd6f24be35e20
Islam Kambarov <ikambarov@yahoo.com>
2021-04-14 01:46:20 +0000

    Parameterize

--- Jan-2021/prepare-worker.groovy
+++ Jan-2021/prepare-worker.groovy
@@ -1,13 +1,19 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Enter IP Address', name: 'IPADDRESS', trim: true)
+        ])
+    ])
+
 node{
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
         stage("Install git"){
             sh """
-                ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.236.193.148 yum install git -y
+                ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.IPADDRESS} yum install git -y
             """
         }   
         stage("Install Java"){
             sh """
-                ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.236.193.148 yum install java-1.8.0-openjdk-devel -y
+                ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.IPADDRESS} yum install java-1.8.0-openjdk-devel -y
             """
         }   
     }
