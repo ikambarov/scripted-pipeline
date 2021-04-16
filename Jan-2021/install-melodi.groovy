e71049f8d2be88332f191f4697e4af20826cd5f3
Islam Kambarov <ikambarov@yahoo.com>
2021-04-16 00:20:08 +0000

    Use Ansible Plugin

--- Jan-2021/install-melodi.groovy
+++ Jan-2021/install-melodi.groovy
@@ -8,13 +8,8 @@ node{
     stage("Pull Repo"){
         git 'https://github.com/ikambarov/ansible-melodi.git'
     }
-
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-        stage("Ansible"){
-            sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook --private-key $SSHKEY -i "${params.IPADDRESS}," main.yml
-            """
-        }
+    
+    stage("Ansible"){
+        ansiblePlaybook credentialsId: 'jenkins-master-ssh-key', disableHostKeyChecking: true, inventory: "${params.IPADDRESS},", playbook: 'main.yml'
     }
 }
