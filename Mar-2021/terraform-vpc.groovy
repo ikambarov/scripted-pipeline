8f7cfea58a6cc40bae6a0383448a6bc84db00a24
Islam Kambarov <ikambarov@yahoo.com>
2021-08-19 01:36:58 +0000

    Update region per environment

--- Mar-2021/terraform-vpc.groovy
+++ Mar-2021/terraform-vpc.groovy
@@ -1,4 +1,19 @@
-properties([parameters([choice(choices: ['apply', 'destroy'], name: 'input')])])
+properties([
+    parameters([
+        choice(choices: ['dev', 'qa', 'prod'], name: 'environment'),
+        choice(choices: ['apply', 'destroy'], name: 'input')
+    ])
+])
+
+if(params.environment == "dev"){
+    aws_region_var = "us-east-1"
+}
+else if(params.environment == "qa"){
+    aws_region_var = "us-east-2"
+}
+else{
+    aws_region_var = "us-west-2"
+}
 
 node("terraform"){
     
@@ -6,35 +21,34 @@ node("terraform"){
         git 'https://github.com/ikambarov/terraform-vpc.git'
     }
     
-    withEnv(['AWS_REGION=us-east-1']) {
+    withEnv(["AWS_REGION=${aws_region_var}"]) {
         withCredentials([usernamePassword(credentialsId: 'aws-key', passwordVariable: 'AWS_SECRET_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
             stage("Terraform init"){
-                sh '''
+                sh """
+                    sh setenv.sh ${params.environment}.tfvars
                     terraform init
-                '''
+                """
             }
 
-            AWS_REGION
-
             stage("Terraform plan"){
-                sh '''
-                    terraform plan -var-file dev.tfvars
-                '''
+                sh """
+                    terraform plan -var-file ${params.environment}.tfvars
+                """
             }
 
             if(params.input == "apply"){
                 stage("Terraform apply"){
-                    sh '''
-                        terraform apply -var-file dev.tfvars -auto-approve
-                    '''
+                    sh """
+                        terraform apply -var-file ${params.environment}.tfvars -auto-approve
+                    """
                 }
             }
 
             else{
                 stage("Terraform destroy"){
-                    sh '''
-                        terraform destroy -var-file dev.tfvars -auto-approve
-                    '''
+                    sh """
+                        terraform destroy -var-file ${params.environment}.tfvars -auto-approve
+                    """
                 }
             }
         }
