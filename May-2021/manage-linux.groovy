716bc850a36f0dc6d9028fb36d2dcec0dad82fdd
Islam Kambarov <ikambarov@yahoo.com>
2021-09-11 22:05:55 +0000

    v2

--- May-2021/manage-linux.groovy
+++ May-2021/manage-linux.groovy
@@ -1,9 +1,15 @@
+properties([
+    parameters([
+        string(description: 'Provide Linux VM IP', name: 'IPADDRESS', trim: true)
+        ])
+    ])
+
 node("worker1"){
     stage("Intall Epel"){
         withCredentials([sshUserPrivateKey(credentialsId: 'masterkey', keyFileVariable: 'SSHKEY', usernameVariable: 'SSHUSERNAME')]) {
-            sh '''
-                ssh -o StrictHostKeyChecking=no -i $SSHKEY root@TEST-VM-IP "hostname"
-            '''
+            sh """
+                ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.IPADDRESS} 'hostname'
+                """
         }        
     }
 }
