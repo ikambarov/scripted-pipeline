dde280f9fc3cb74bf886f88f91c53b39559092e6
Islam Kambarov <ikambarov@yahoo.com>
2021-09-18 19:58:37 +0000

    Add parameters

--- May-2021/aws-packer.groovy
+++ May-2021/aws-packer.groovy
@@ -1,9 +1,27 @@
+properties([
+    parameters([
+        choice(choices: ['dev', 'qa', 'prod'], description: 'Choose an ENV from the list', name: 'environment')
+        ])
+    ])
+
+if(params.environment == 'dev'){
+    region="us-east-1"
+}
+else if(params.environment == 'qa'){
+    region="us-east-2"
+}
+else{
+    region="us-west-2"
+}
+
 node("worker1"){
     stage("Pull code"){
         git 'https://github.com/ikambarov/packer.git'
     }
 
-    withEnv(['AWS_REGION=us-east-1', 'PACKER_AMI_NAME=testimage1']) {
+    image_name="$params.environment-apache-${UUID.randomUUID().toString()}"
+
+    withEnv(["AWS_REGION=$region", "PACKER_AMI_NAME=$image_name"]) {
         withCredentials([usernamePassword(credentialsId: 'aws-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
 
             stage("Validate"){
@@ -19,4 +37,7 @@ node("worker1"){
             }
         }
     }
+    // stage("Create Instance"){
+    //     build 'terraform-ec2'
+    // }
 }
