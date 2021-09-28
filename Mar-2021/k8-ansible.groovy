e447c12573406d0cc6f350863ff8d47f78db5abc
Islam Kambarov <ikambarov@yahoo.com>
2021-09-27 21:03:30 -0500

    add k8-ansible.groovy

---
+++ Mar-2021/k8-ansible.groovy
@@ -0,0 +1,24 @@
+def podname="ansible"
+
+def podtemplate="""
+metadata:
+  labels:
+    run: $podname
+spec:
+  containers:
+  - image: yourname/ansible
+    name: $podname
+    args:
+    - sleep
+    - "100000"
+"""
+
+podTemplate(name: podname, label: podname, yaml: podtemplate, showRawYaml: false) {
+    node(podname){
+        stage('Check version'){
+            sh '''
+                ansible --version
+            '''
+        }
+    }
+}
