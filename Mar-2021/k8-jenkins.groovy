865899b6b1284cfbac293b502ac3cdd536927168
Islam Kambarov <ikambarov@yahoo.com>
2021-10-04 18:44:54 -0500

    Add k8-jenkins.groovy

---
+++ Mar-2021/k8-jenkins.groovy
@@ -0,0 +1,39 @@
+def podname="k8-tools"
+
+def podtemplate="""
+metadata:
+  labels:
+    run: $podname
+spec:
+  serviceAccountName: jenkins
+  affinity:
+    podAntiAffinity:
+    requiredDuringSchedulingIgnoredDuringExecution:
+    - labelSelector:
+      matchExpressions:
+      - key: component
+        operator: In
+        values:
+        - jenkins-jenkins-master
+      topologyKey: "kubernetes.io/hostname"
+  containers:
+  - image: ikambarov/k8-tools
+    name: $podname
+    args:
+    - sleep
+    - "100000"
+"""
+
+podTemplate(name: podname, label: podname, yaml: podtemplate, showRawYaml: false) {
+    node(podname){
+        stage('Check version'){
+            container(podname){
+                sh '''
+                    kubectl version
+                    kubectl get pods
+                    helm version
+                '''
+            }           
+        }
+    }
+}
