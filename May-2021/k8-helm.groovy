41c3cd6f21433cd0212516e307eacd540a7a82e0
Islam Kambarov <ikambarov@yahoo.com>
2021-11-18 19:53:02 -0600

    Deploy using Helm

---
+++ May-2021/k8-helm.groovy
@@ -0,0 +1,31 @@
+def podtemplate = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    type: k8-tools
+spec:
+  serviceAccountName: k8-tools-sa
+  containers:
+  - image: ikambarov/k8-tools
+    name: k8-tools
+    args:
+    - sleep
+    - "100000"
+'''
+
+podTemplate(label: 'k8-tools', name: 'k8-tools', namespace: 'tools', yaml: podtemplate, showRawYaml: false) {
+    node("k8-tools"){
+        container("k8-tools"){
+            stage("Pull repo"){
+                git 'https://github.com/ikambarov/flaskex-chart.git'
+            }
+
+            stage("deploy"){
+                sh '''
+                    helm upgrade --install myapp . -n default
+                '''
+            }
+        }
+    }
+}
