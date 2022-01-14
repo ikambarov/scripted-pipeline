e26b843b9313943a04956be7b8fd8d3ee60c3411
Islam Kambarov <ikambarov@yahoo.com>
2022-01-14 02:41:47 +0000

    Use Ansible Plugin

--- Oct-2021/flaskex.groovy
+++ Oct-2021/flaskex.groovy
@@ -4,19 +4,21 @@ properties([
     ])
 ])
 
-node {
-    stage("Pull Ansible Repo"){
-        git 'https://github.com/ikambarov/ansible-Flaskex.git'
-    }
-    
-    stage("Run Ansible"){
-        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER')]) {
-            withEnv(['FLASKEX_REPO=https://github.com/ikambarov/Flaskex.git', 'FLASKEX_BRANCH=master']) {
-                sh """
-                    export ANSIBLE_HOST_KEY_CHECKING=False
-                    ansible-playbook --private-key $SSH_KEY  -i '${params.HOSTIP},'  main.yml -u $SSH_USER
-                """
+if( params.HOSTIP.length() >= 7 ) {
+    node {
+        stage("Pull Ansible Repo"){
+            git 'https://github.com/ikambarov/ansible-Flaskex.git'
+        }
+        
+        stage("Run Ansible"){
+            withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER')]) {
+                withEnv(['FLASKEX_REPO=https://github.com/ikambarov/Flaskex.git', 'FLASKEX_BRANCH=master']) {
+                    ansiblePlaybook credentialsId: 'jenkins-master-key', disableHostKeyChecking: true, inventory: "${params.HOSTIP},", playbook: 'main.yml'
+                }
             }
         }
     }
+}
+else {
+    error 'Please enter valid IP'
 }
