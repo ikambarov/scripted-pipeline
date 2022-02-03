bff2120114fe58f547e35c8cf3024ee72935cebb
Islam Kambarov <ikambarov@yahoo.com>
2022-02-03 02:13:52 +0000

    Parameterize

--- Aug-2021/ansible.groovy
+++ Aug-2021/ansible.groovy
@@ -1,10 +1,20 @@
+properties([
+    parameters([
+        string(description: 'Enter an IP address:', name: 'ip', trim: true)
+    ])
+])
+
 node('ansible'){
-    stage("Test"){
+    stage("Pull Repo"){
+        git 'https://github.com/ikambarov/ansible-melodi.git'
+    }
+
+    stage("Deploy"){
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-key', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER')]) {
-            sh '''
+            sh """
                 export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible --private-key $SSH_KEY -i "159.65.226.38," all -m ping -u $SSH_USER
-            '''
+                ansible-playbook --private-key $SSH_KEY -i '${ params.ip },' -u $SSH_USER main.yml
+            """
         }
     }
 }
