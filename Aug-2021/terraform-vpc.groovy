31ddbd7c6cbc31137797a36e6df11c84ca33e700
Islam Kambarov <ikambarov@yahoo.com>
2022-02-05 18:04:59 +0000

    Updated version

---
+++ Aug-2021/terraform-vpc.groovy
@@ -0,0 +1,42 @@
+properties([
+    parameters([
+        choice(choices: ['dev', 'qa', 'prod'], description: 'Choose environment', name: 'environment')
+    ])
+])
+
+if( params.environment == "dev" ){
+    aws_region_var = "us-east-1"
+}
+else if( params.environment == "qa" ){
+    aws_region_var = "us-east-2"
+}
+else if( params.environment == "prod" ){
+    aws_region_var = "us-west-2"
+}
+
+node('terraform'){
+    stage("Pull Code"){
+        git 'https://github.com/ikambarov/terraform-vpc.git'
+    }
+
+    withCredentials([usernamePassword(credentialsId: 'aws-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        withEnv(["AWS_REGION=${aws_region_var}"]) {
+            stage("Terraform Init"){
+                sh """
+                    #!/bin/bash
+                    source ./setenv.sh ${params.environment}.tfvars
+
+                    terraform init
+                """
+            }
+
+            stage("Terraform Plan"){
+                sh "terraform plan -var-file ${params.environment}.tfvars"
+            }
+            
+            stage("Terraform Apply"){
+                sh "terraform apply -var-file ${params.environment}.tfvars -auto-approve"
+            }
+        }        
+    }
+}
