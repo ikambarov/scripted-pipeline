34dba2051d38623e09d7f5cca090e25a229e27de
Islam Kambarov <ikambarov@yahoo.com>
2022-02-17 00:57:04 +0000

    Add k8s-deploy.groovy

---
+++ Aug-2021/k8s-deploy.groovy
@@ -0,0 +1,28 @@
+podyaml = '''
+metadata:
+  labels:
+    run: k8s
+  name: k8s
+spec:
+  serviceAccount: agent-sa
+  containers:
+  - args:
+    - sleep
+    - "1000000"
+    image: ikambarov/k8-tools
+    name: k8s
+'''
+
+podTemplate(cloud: 'kubernetes', label: 'k8s', name: 'k8s', namespace: 'jenkins', yaml: podyaml, showRawYaml: false) {
+    node('k8s'){
+        container('k8s'){
+            stage('Deploy'){
+                sh """
+                    kubectl create deployment flaskex --image=ikambarov/flaskex --port=5000 -n default -o yaml --dry-run | kubectl apply --force -f - 
+                    
+                    kubectl expose deployment flaskex --target-port=5000 --type=NodePort -n default -o yaml --dry-run | kubectl apply --force -f - 
+                """
+            }
+        }
+    }
+}
