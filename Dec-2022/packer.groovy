f2040312f1bd33cfb5d2ef25fac788e607e336bf
Islam Kambarov <ikambarov@yahoo.com>
2022-04-07 01:20:29 +0000

    Use different environments

--- Dec-2022/packer.groovy
+++ Dec-2022/packer.groovy
@@ -1,10 +1,29 @@
+properties([
+    parameters([
+        choice(choices: ['dev', 'qa', 'prod'], description: 'Choose an Environment', name: 'environment'),
+        ])
+    ])
+
+if( params.environment == 'dev' ) {
+    vpc_region = "us-east-1"
+}
+
+else if( params.environment == 'qa' ) {
+    vpc_region = "us-east-2"
+}
+
+else {
+    vpc_region = "us-west-2"
+}
+
 node {
     stage("Pull Repo"){
         git 'https://github.com/ikambarov/packer.git'
     }
-
+    
+    def ami_name = "apache-${UUID.randomUUID().toString()}"
     withCredentials([usernamePassword(credentialsId: 'aws-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        withEnv(['AWS_REGION=us-east-1', 'PACKER_AMI_NAME=apache']) {
+        withEnv(["AWS_REGION=${vpc_region}", "PACKER_AMI_NAME=${ami_name}"]) {
             stage("Validate"){
                 sh """
                     packer validate apache.json
