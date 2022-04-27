70e821b67f0a7f6a3a030a97f6610098b5e45a55
Islam Kambarov <ikambarov@yahoo.com>
2022-04-26 19:40:20 -0500

    Create k8s-terraform.groovy

---
+++ Oct-2021/k8s-terraform.groovy
@@ -0,0 +1,19 @@
+pod = '''apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: terraform
+  name: terraform
+spec:
+  containers:
+  - image: ikambarov/terraform:0.14
+    name: terraform
+'''
+
+podTemplate(cloud: 'kubernetes', label: 'terraform', name: 'terraform', yaml: pod ) {
+    node('terraform'){
+        stage("Check Terraform Version"){
+            sh "terraform version"
+        }
+    }
+}
