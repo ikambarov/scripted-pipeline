198b0115657eb8f62104e1c79910cc25b56c74a1
Islam Kambarov <ikambarov@yahoo.com>
2022-04-28 19:30:14 -0500

    Deploy Chart

---
+++ Oct-2021/k8s-helm.groovy
@@ -0,0 +1,28 @@
+pod = '''apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: helm
+  name: helm
+spec:
+  containers:
+  - image: ikambarov/k8s-tools
+    name: helm
+  serviceAccountName: agent-sa
+'''
+
+podTemplate(cloud: 'kubernetes', showRawYaml: false, label: 'helm', name: 'helm', yaml: pod ) {
+    node('helm'){
+        container('helm'){
+            stage("Pull Chart"){
+                git("https://github.com/ikambarov/flaskex-chart.git")
+            }
+
+            stage("Test"){
+                sh """
+                    helm install -n default flaskex . 
+                """
+            }
+        }
+    }
+}
