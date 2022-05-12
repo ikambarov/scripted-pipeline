63122dfd9d175ea848412f28dcbf9f0f8c87bea9
Islam Kambarov <ikambarov@yahoo.com>
2022-05-12 23:20:13 +0000

    Parameterize job

--- Feb-2022/ssh.groovy
+++ Feb-2022/ssh.groovy
@@ -1,7 +1,13 @@
+properties([
+    parameters([
+        string(description: 'Enter Linux IP Address', name: 'IP', trim: true)
+        ])
+    ])
+
 node {
     stage('Connect to Test VM') {
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-key', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USERNAME')]) {
-            sh "ssh -i $SSH_KEY -o StrictHostKeyChecking=no $SSH_USERNAME@159.89.178.141"
+            sh "ssh -i $SSH_KEY -o StrictHostKeyChecking=no $SSH_USERNAME@$params.IP"
         }
     }
 }
