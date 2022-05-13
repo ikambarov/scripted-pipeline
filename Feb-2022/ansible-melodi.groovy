5b7ffb1f59c76a5466a41ee57a318d03d9438144
Islam Kambarov <ikambarov@yahoo.com>
2022-05-13 00:42:19 +0000

    Deploy ansible-melodi

---
+++ Feb-2022/ansible-melodi.groovy
@@ -0,0 +1,17 @@
+properties([
+    parameters([
+        string(description: 'Enter Linux IP Address', name: 'IP', trim: true)
+        ])
+    ])
+
+node {
+    stage("Pull Playbook"){
+        git 'https://github.com/ikambarov/ansible-melodi.git'
+    }
+    
+    stage("Test"){
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-key', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USERNAME')]) {
+            sh "ansible-playbook --private-key=$SSH_KEY -i '$params.IP,' -u $SSH_USERNAME main.yml"
+        }
+    }
+}
