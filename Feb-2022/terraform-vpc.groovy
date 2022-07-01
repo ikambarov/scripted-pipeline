089ffc517b5baf6f86bb6860c376a1feb91d679a
Islam Kambarov <ikambarov@yahoo.com>
2022-06-30 20:32:51 -0500

    Final version

--- Feb-2022/terraform-vpc.groovy
+++ Feb-2022/terraform-vpc.groovy
@@ -1,26 +1,44 @@
+properties([
+    parameters([
+        choice(choices: ['dev', 'qa', 'prod'], description: 'Please choose an ENV', name: 'environment')
+    ])
+])
+
+if(params.environment == 'dev'){
+    region = "us-east-1"
+}
+else if(params.environment == 'qa'){
+    region = "us-east-2"
+}
+else{
+    region = "us-west-2"
+}
+
 node("terraform"){
     stage("Pull Repo"){
         git 'https://github.com/ikambarov/terraform-vpc.git'
     }
 
-    withCredentials([usernamePassword(credentialsId: 'aws-creds', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        stage("Init"){
-            sh """
-                bash setenv.sh dev.tfvars
-                terraform init
-            """
-        }
-        
-        stage("Plan"){
-            sh """
-                terraform plan -var-file dev.tfvars
-            """
-        }
+    withEnv(["AWS_REGION=${ region }"]) {
+        withCredentials([usernamePassword(credentialsId: 'aws-creds', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+            stage("Init"){
+                sh """
+                    bash setenv.sh ${ params.environment }.tfvars
+                    terraform init
+                """
+            }
+            
+            stage("Plan"){
+                sh """
+                    terraform plan -var-file ${ params.environment }.tfvars
+                """
+            }
 
-        stage("Apply"){
-            sh """
-                terraform apply -var-file dev.tfvars -auto-approve
-            """
+            stage("Apply"){
+                sh """
+                    terraform apply -var-file ${ params.environment }.tfvars -auto-approve
+                """
+            }
         }
-    }
+    } 
 }
