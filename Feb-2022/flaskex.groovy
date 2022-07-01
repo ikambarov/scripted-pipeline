4a0fc4564149134e6473e4fd5419f20cf0d67848
Islam Kambarov <ikambarov@yahoo.com>
2022-06-30 19:24:23 -0500

    Update

--- Feb-2022/flaskex.groovy
+++ Feb-2022/flaskex.groovy
@@ -9,21 +9,23 @@ node {
         git 'https://github.com/ikambarov/ansible-Flaskex.git'
     }
 
-    stage("Pull repo"){
-        withEnv(['FLASKEX_REPO=https://github.com/anfederico/flaskex.git', 'FLASKEX_BRANCH=master']) {
-            ansiblePlaybook credentialsId: 'jenkins-key', inventory: "$params.IP,", playbook: 'pull_repo.yml'
+    withEnv(['ANSIBLE_HOST_KEY_CHECKING=False']){
+        stage("Pull repo"){
+            withEnv(['FLASKEX_REPO=https://github.com/anfederico/flaskex.git', 'FLASKEX_BRANCH=master']) {
+                ansiblePlaybook credentialsId: 'jenkins-key', inventory: "$params.IP,", playbook: 'pull_repo.yml'
+            }
         }
-    }
 
-    stage("Install Python"){
-        ansiblePlaybook credentialsId: 'jenkins-key', inventory: "$params.IP,", playbook: 'install_python.yml'
-    }
+        stage("Install Python"){
+            ansiblePlaybook credentialsId: 'jenkins-key', inventory: "$params.IP,", playbook: 'install_python.yml'
+        }
 
-    stage("Install Prerequisites"){
-        ansiblePlaybook credentialsId: 'jenkins-key', inventory: "$params.IP,", playbook: 'prerequisites.yml'
-    }
-    
-    stage("Start App"){
-        ansiblePlaybook credentialsId: 'jenkins-key', inventory: "$params.IP,", playbook: 'start_app.yml'
+        stage("Install Prerequisites"){
+            ansiblePlaybook credentialsId: 'jenkins-key', inventory: "$params.IP,", playbook: 'prerequisites.yml'
+        }
+        
+        stage("Start App"){
+            ansiblePlaybook credentialsId: 'jenkins-key', inventory: "$params.IP,", playbook: 'start_app.yml'
+        }
     }
 }
