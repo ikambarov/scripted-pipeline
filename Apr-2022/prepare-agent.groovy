cc39aea263dc95e6ab662555ebf1ca84d561debf
Islam Kambarov <ikambarov@yahoo.com>
2022-07-12 01:53:55 +0000

    Use parameter

--- Apr-2022/prepare-agent.groovy
+++ Apr-2022/prepare-agent.groovy
@@ -1,7 +1,13 @@
+properties([
+    parameters([
+        string(description: 'Enter IP Address', name: 'ip')
+    ])
+])
+
 node {
     stage("Install Java"){
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USER')]) {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USER@137.184.219.26 yum install java-1.8.0-openjdk-devel -y"
+            sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USER@${params.ip} yum install java-1.8.0-openjdk-devel -y"
         }
     }
 }
