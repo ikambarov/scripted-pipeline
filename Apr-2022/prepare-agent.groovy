5c5773997ec37309990eb4c31de57d3ec7ad51bc
Islam Kambarov <ikambarov@yahoo.com>
2022-07-13 23:12:22 +0000

    Install git

--- Apr-2022/prepare-agent.groovy
+++ Apr-2022/prepare-agent.groovy
@@ -5,9 +5,13 @@ properties([
 ])
 
 node {
-    stage("Install Java"){
-        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USER')]) {
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USER')]) {
+        stage("Install Java"){
             sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USER@${params.ip} yum install java-1.8.0-openjdk-devel -y"
+        }
+        
+        stage("Install Git"){
+            sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USER@${params.ip} yum install git -y"
         }
     }
 }
