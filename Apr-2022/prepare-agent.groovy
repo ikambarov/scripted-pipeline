67c2a95d419056043c61310d6eb56ee8f61470d8
Islam Kambarov <ikambarov@yahoo.com>
2022-07-14 00:06:55 +0000

    Install ansible

--- Apr-2022/prepare-agent.groovy
+++ Apr-2022/prepare-agent.groovy
@@ -9,9 +9,13 @@ node {
         stage("Install Java"){
             sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USER@${params.ip} yum install java-1.8.0-openjdk-devel -y"
         }
-        
+
         stage("Install Git"){
             sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USER@${params.ip} yum install git -y"
+        }
+
+        stage("Install Ansible"){
+            sh "ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USER@${params.ip} yum install epel-release -y && yum install ansible -y"
         }
     }
 }
