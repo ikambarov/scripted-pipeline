7eb395f58ca9f7359ba2695efd0510c26264d2c3
Islam Kambarov <ikambarov@yahoo.com>
2022-08-06 17:02:04 -0500

    Steps 1-3

---
+++ Feb-2022/k8s-deployment.groovy
@@ -0,0 +1,44 @@
+podtemplate = '''apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: k8s-tools
+  name: k8s-tools
+spec:
+  volumes:
+  - name: docker-sock
+    hostPath:
+      path: /var/run/docker.sock
+  containers:
+  - args:
+    - sleep
+    - "10000000"
+    image: ikambarov/k8s-tools
+    name: k8s-tools
+    volumeMounts:
+    - mountPath: /var/run/docker.sock
+      name: docker-sock
+      '''
+
+podTemplate(cloud: 'kubernetes', label: 'k8s-tools', showRawYaml: false, yaml: podtemplate ) {
+    node('k8s-tools'){
+        container('k8s-tools'){
+            stage("Pull repo"){
+                git 'https://github.com/ikambarov/Flaskex-docker.git'
+            }
+
+            withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                stage("Build"){
+                    sh 'docker build -t $DOCKER_USERNAME/flaskex .'
+                }
+
+                stage("Push"){
+                    sh '''
+                        docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+                        docker push $DOCKER_USERNAME/flaskex 
+                    '''
+                }
+            }
+        }
+    }
+}
