2bfae00b2ba03e4a4675b93a97e1577117de20f7
Islam Kambarov <ikambarov@yahoo.com>
2022-10-03 20:43:34 -0500

    Add terraform pipeline

---
+++ Apr-2022/terraform.groovy
@@ -0,0 +1,38 @@
+template = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  name: terraform
+spec:
+  containers:
+  - image: ikambarov/terraform:0.14
+    name: terraform
+    '''
+
+tfvars = '''
+environment   = "dev"	
+region        = "us-east-1"	
+public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXUI8Mt0W/65CPA5rnR4auE8qVb08c6qR9Ca0yQaz9xM6EuShYX8jmktYbrdCIkZTMXbRF58CkWID/NHjYX4ZWZHwLi5uf2RfQegF67+kv6yJ2cgG4AsxUmWqlznxvm9615r8tpzBkKgsya58H+4aPRKqLJmhRm3ZZCa7t2HE7S+RR7fq+WtaQ3BMaKog9AVfHSEP8Gp4Ho7WUv5YlLXu5hlYC+m2oxrSCqXRFIhDtDuyphkzS93gDy8EVBkWnJFkoXT2LbVydcJaNCpEdjB1YFEEc1kMOXCAZ0w5N8PiWgdlY0lPeRXdH1RLX+WCM5FVOT9ujrq8PTQSYIkl2pek3 ikambarov@Islams-MacBook-Pro.local"	
+ami_id        = "ami-02eac2c0129f6376b"
+'''
+
+podTemplate(cloud: 'kubernetes', label: 'terraform', showRawYaml: false, yaml: template) {
+    node("terraform"){
+        container("terraform"){
+            stage("Pull Code"){
+                git 'https://github.com/ikambarov/terraform-ec2.git'
+            }
+
+            stage("Init"){
+                sh "terraform init"
+            }
+
+            withCredentials([usernamePassword(credentialsId: 'aws-creds', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+                stage("Apply"){
+                    writeFile file: 'my.tfvars', text: tfvars
+                    sh "terraform apply -var-file my.tfvars -auto-approve"
+                }
+            }
+        }
+    }
+}
